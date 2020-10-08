e036e87f051dd5b75f60884fb939979c2fd5cc71
fareselkhouli <fareselkhouli@gmail.com>
2020-10-08 00:38:32 -0400

    implented colors for start/end positions. Implented goal/start setting from GUI.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,6 +8,8 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import sample.back.*;
 
+import java.awt.*;
+
 public class Controller {
 
     //inject FXML Objects
@@ -23,7 +25,17 @@ public class Controller {
     private TextField pathField;
     @FXML
     private Pane gridPane;
+    @FXML
+    private Button startButton;
+    @FXML
+    private Button goalButton;
+    @FXML
+    private TextField goalField;
+    @FXML
+    private TextField startField;
 
+    //Grid Object
+    Grid g = new Grid();
 
     /**
      * This method generates a random map after the generateMe button is clicked
@@ -31,10 +43,10 @@ public class Controller {
     @FXML
     public void generateME(){
 
-        Grid newMap = new Grid();
-        newMap.generateGrid();
-        newMap.writeToFile("map4.txt");
-        buildGrid(newMap);
+        Grid g = new Grid();
+        g.generateGrid();
+        g.writeToFile("map4.txt");
+        buildGrid(g);
 
     }
 
@@ -47,7 +59,7 @@ public class Controller {
         String filepath = pathField.getText();
         if (filepath.isEmpty()) return;
 
-        Grid g = new Grid(filepath);
+        g = new Grid(filepath);
         buildGrid(g);
 
 
@@ -61,19 +73,30 @@ public class Controller {
      * Hard to Traverse - Gray
      * Regular Cell w/ Highway - Blue
      * Hard to Traverse w/ Highway - Dark Blue
+     * Start - Lime Green
+     * Goal - Red
+     * Path - Yellow
      * @param g - Input Grid object
      */
     public void buildGrid(Grid g){
+        //Clear Pane
         gridPane.getChildren().clear();
 
+        //Initialize variables
         int width = g.columnSize;
         int height = g.rowSize;
-        int rectangleW = 6;
-        int rectangleH = 6;
-
+        int rectangleW = 8;
+        int rectangleH = 8;
+        Point start = g.getStart();
+        int startX =(int) start.getX();
+        int startY =(int) start.getY();
+        Point goal = g.getEndVertex();
+        int goalX = (int) goal.getX();
+        int goalY = (int) goal.getY();
 
         Rectangle[][] cells = new Rectangle[(int) width][(int) height];
 
+        //Draw Rectangles in Pane
         for (int j = 0; j < height; ++j){
             //draw row
             for (int i = 0; i < width; ++i){
@@ -104,5 +127,38 @@ public class Controller {
                 gridPane.getChildren().add(cells[i][j]);
             }
         }
+
+        //Color start and end pos
+        cells[startY][startX].setFill(Color.LIMEGREEN);
+        cells[goalY][goalX].setFill(Color.RED);
     }
+
+    /**
+     * Sets the grid's goal and updates grid visual
+     */
+    @FXML
+    public void setGridGoal(){
+        String input = goalField.getText();
+        int goalX = input.charAt(0);
+        int goalY = input.charAt(2);
+
+
+        g.setEnd(goalX,goalY);
+        buildGrid(g);
+    }
+
+    /**
+     * Sets the grid's start and updates grid visual
+     */
+    @FXML
+    public void setGridStart(){
+        String input = startField.getText();
+        int startX = Character.getNumericValue(input.charAt(0));
+        int startY = Character.getNumericValue(input.charAt(2));
+
+        g.setStart(startX,startY);
+        buildGrid(g);
+    }
+
+
 }
--- src/sample/back/Grid.java
+++ src/sample/back/Grid.java
@@ -484,19 +484,19 @@ public class Grid{
             return this.map[x][y].getType();
         }
     }
-    Point getStart(){
+    public Point getStart(){
         return startVertex;
     }
 
-    void setStart(int x, int y){
+    public void setStart(int x, int y){
         this.startVertex = new Point(x,y);
     }
 
-    void setEnd(int x, int y){
+    public void setEnd(int x, int y){
         this.endVertex = new Point(x,y);
     }
 
-    Point getEndVertex(){
+    public Point getEndVertex(){
         return endVertex;
     }
 
